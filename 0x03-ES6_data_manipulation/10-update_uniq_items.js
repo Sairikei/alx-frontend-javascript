5ec12eb8193558113408cf058ff588893b4559c2
Sairikei <gedionsaiyuah@gmail.com>
2022-06-16 19:36:25 +0300

    data manipulation

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,11 @@
-export default function updateUniqueItems(groceries) {
-  if (!(groceries instanceof Map)) throw Error('Cannot process');
-
-  groceries.forEach((value, key) => {
-    if (value === 1) groceries.set(key, 100);
-  });
-
-  return groceries;
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw Error('Cannot process');
+  }
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+  return map;
 }
